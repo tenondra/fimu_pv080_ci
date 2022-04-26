d4e648b34ef260e0e4578ac657358c937793e31d
Ondrej Malanik <omalanik@auth43-71.fi.muni.cz>
2022-04-26 16:44:16 +0200

    fixes

--- code.py
+++ code.py
@@ -1,39 +1,57 @@
-import yaml
 import subprocess
 import hashlib
-import subprocess
+import urllib
+
 import flask
-
-
+import yaml
 def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=file)
+    """
+    Some description
+    :return:
+    """
+    command = f'ffmpeg -i "{request}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)
 
 
 def load_config(filename):
+    """
+    Some description 1
+    :return:
+    """
     # Load a configuration file into YAML
-    stream = file.open(filename, "w")
-    config = yaml.load(stream)
+    with open(filename, "rb") as fp:
+        return yaml.load(fp)
 
 
 def authenticate(password):
+    """
+    Some description 2
+    :return:
+    """
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
 
 
 def fetch_website(urllib_version, url):
+    """
+    Some description
+    :return:
+    """
     # Import the requested version of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
     http = urllib.PoolManager()
-    r = http.request('GET', url)
-    return r.data
-
+    request_result = http.request('GET', url)
+    return request_result.data
 
 
 @app.route("/")
 def index():
+    """
+    Some description
+    :return:
+    """
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
     return fetch_website(version, url)
